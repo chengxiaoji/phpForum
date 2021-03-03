2ef18b3e3f264054a05a0de174b30282b16bc676
chengxiaoji <2572432678@qq.com>
2021-03-03 17:07:44 +0800

    phpForum 2.8

--- Login.php
+++ Login.php
@@ -1,26 +1,10 @@
 <?php
-$conn=new Connection();
 require 'components/MysqlConnection.php';
-$data = file_get_contents("php://input");
-$data = json_decode($data,true);
-$username= trim($data['name']);
-$password= trim($data['pass']);
-$sql = "select * from forum.forum_user where username='$username' and password='$password'";
-$result = mysqli_query($conn,$sql);
-$row = mysqli_fetch_assoc($result);
-if ($row==null)
-{
-    $code=401;
-    $msg="用户名或密码错误！请输入正确的用户名和密码！";
-}
-else{
-    $code=200;
-    $msg="账号密码正确！";
-    session_start();
-    $_SESSION['userInfo']=$username;
-}
-$str = array(
-    'msg'=>$msg,
-    'code'=>$code,
-);
-echo json_encode($str);
+require 'components/user.php';
+header('Content-Type:text/json;charset=utf-8');
+$info = getUserInfo();
+checkLoginInfo($info);
+checkLogin($info);
+setCookies($info);
+setSessions($info);
+updateLogin($info);
